ff7aeb03d569c852fe64006f14d3e3c956bdf6ae
Gilbert Paderogo <gspaderogo@csu.fullerton.edu>
2020-03-08 13:36:30 -0700

    added and deleted functions from vote-service and post-service

--- post-service.py
+++ post-service.py
@@ -91,3 +91,48 @@ def get_all_recent(numOfEntries):
 
 if __name__ == "__main__":
     app.run()
+
+# Create a new entry
+def create_entry(entry):
+    required_fields = ['id', 'title', 'bodyText', 'community', 'url', 'username', 'datePosted']
+
+    if not all([field in entry for field in required_fields]):
+        raise exceptions.ParseError()
+    try:
+        entry['id'] = queries.create_entry(**entry)
+    except Exception as e:
+        return { 'error': str(e) }, status.HTTP_409_CONFLICT
+
+    return entry, status.HTTP_201_CREATED, {
+        'Location': f'/api/v1/entries/{entry["id"]}'
+    }
+
+# Filter entries given user input
+def filter_entries(query_parameters):
+    id = query_parameters.get('id')
+
+    query = "SELECT * FROM entries WHERE"
+    to_filter = []
+
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if not (id):
+        raise exceptions.NotFound()
+
+    query = query[:-4] + ';'
+
+    results = queries._engine.execute(query, to_filter).fetchall()
+
+    return list(map(dict, results))
+
+# Return entry given an id
+def get_entry_with_id(id):
+    entry = queries.entry_by_id(id=id)
+    if entry:
+        return entry
+    else:
+        raise exceptions.NotFound()
+
+if __name__ == "__main__":
+    app.run()
--- vote-service.py
+++ vote-service.py
@@ -86,40 +86,6 @@ def score_list(identifiers):
     else:
        return { 'message': 'Posts could not be retrieved' }, status.HTTP_400_BAD_REQUEST
 
-# Create a new entry
-def create_entry(entry):
-    required_fields = ['id', 'title', 'bodyText', 'community', 'url', 'username', 'datePosted']
-
-    if not all([field in entry for field in required_fields]):
-        raise exceptions.ParseError()
-    try:
-        entry['id'] = queries.create_entry(**entry)
-    except Exception as e:
-        return { 'error': str(e) }, status.HTTP_409_CONFLICT
-
-    return entry, status.HTTP_201_CREATED, {
-        'Location': f'/api/v1/entries/{entry["id"]}'
-    }
-
-# Filter entries given user input
-def filter_entries(query_parameters):
-    id = query_parameters.get('id')
-
-    query = "SELECT * FROM entries WHERE"
-    to_filter = []
-
-    if id:
-        query += ' id=? AND'
-        to_filter.append(id)
-    if not (id):
-        raise exceptions.NotFound()
-
-    query = query[:-4] + ';'
-
-    results = queries._engine.execute(query, to_filter).fetchall()
-
-    return list(map(dict, results))
-
 # Return entry given an id
 def get_entry_with_id(id):
     entry = queries.entry_by_id(id=id)
