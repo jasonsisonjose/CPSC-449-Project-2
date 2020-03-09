d8b9feaf0a8604d1156aaa74210904b7c4e7ce00
Gilbert Joseph S. Paderogo <gspaderogo@csu.fullerton.edu>
2020-03-09 11:52:16 -0700

    changed port

--- scripts/testRunVoting.sh
+++ scripts/testRunVoting.sh
@@ -4,12 +4,12 @@ curl --verbose \
      --request POST \
      --header 'Content-Type: application/json' \
      --data @identifierList.json \
-     http://localhost:5000/api/v1/votes/scorelist
+     http://localhost:5100/api/v1/votes/scorelist
 
 curl --verbose \
      --request PUT \
-     http://localhost:5000/api/v1/votes/1
+     http://localhost:5100/api/v1/votes/1
 
 curl --verbose \
      --request PATCH \
-     http://localhost:5000/api/v1/votes/1
+     http://localhost:5100/api/v1/votes/1
