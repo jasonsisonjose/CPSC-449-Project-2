0e7042f888d55322ce4a732dc2085b6376124f2e
NicWebster96 <NicWebster96@gmail.com>
2020-03-09 11:58:49 -0700

    delete scripts

--- identifierList.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl --verbose \
-     --request POST \
-     --header 'Content-Type: application/json' \
-     --data @identifierList.json \
-     http://localhost:5100/api/v1/votes/scorelist
