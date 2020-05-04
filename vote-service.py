51ff9745a3ced2ce92d549feee5efc85f1e6f093
Gilbert Joseph Paderogo <gspaderogo@csu.fullerton.edu>
2020-05-04 02:07:37 -0700

    redis now works with checking votes, can upvote and downvote

--- vote-service.py
+++ vote-service.py
@@ -9,26 +9,44 @@ import pugsql
 from dotenv import load_dotenv
 load_dotenv()
 
-# Create instance of Flask using the Flask API
+# Import Redis
+from flask_redis import Redis
+
+# ---- Initialize Redis using Flask ----
 app = flask_api.FlaskAPI(__name__)
-app.config.from_envvar('APP_CONFIG')
+app.config['REDIS_HOST'] = 'localhost'
+# default port
+app.config['REDIS_PORT'] = 6379
+app.config['REDIS_DB'] = 0
+r_server = Redis(app)
+#app.config.from_envvar('APP_CONFIG')
 
-queries = pugsql.module('queries/')
-queries.connect(app.config['DATABASE_URL'])
+#queries = pugsql.module('queries/')
+#queries.connect(app.config['DATABASE_URL'])
 
 
-@app.cli.command('init')
-def init_db():
-    with app.app_context():
-        db = queries._engine.raw_connection()
-        with app.open_resource('entries.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+#def init_db():
+#    with app.app_context():
+#        db = queries._engine.raw_connection()
+#        with app.open_resource('entries.sql', mode='r') as f:
+#            db.cursor().executescript(f.read())
+#        db.commit()
+
+
+# ---- Create Keys/Data for Votes Table ----
+r_server.hset(0,"upvotes",1)
+r_server.hset(0,"downvotes",2)
+
+r_server.hset(1,"upvotes",69)
+r_server.hset(1,"downvotes",7)
+
+r_server.hset(2,"upvotes",420)
+r_server.hset(2,"downvotes",4)
+
 
 # Home page
 @app.route('/', methods=['GET'])
 def home():
-    #return 'Welcome to Nic\'s localhost'
     return '''<h1>Welcome to Fake Reddit!</h1>'''
 
 
@@ -45,7 +63,8 @@ def get_top_scoring(numOfEntries):
 @app.route('/api/v1/votes/<int:id>', methods=['GET', 'PUT', 'PATCH'])
 def report_votes(id):
     if request.method == 'GET':
-        report_votes = queries.report_votes(id=id)
+        #report_votes = queries.report_votes(id=id)
+        report_votes = r_server.hmget(id,"upvotes","downvotes")
         if report_votes:
             return report_votes
         else:
@@ -53,15 +72,17 @@ def report_votes(id):
 
     # using PUT method to upvote entry
     elif request.method == 'PUT':
-        up_vote_entry = queries.up_vote_entry(id=id)
+        #up_vote_entry = queries.up_vote_entry(id=id)
+        up_vote_entry = upvoteEntry(r_server,id)
         if up_vote_entry:
             return { 'message': f'Entry with id {id} has been upvoted' }, status.HTTP_200_OK
         else:
             return { 'message': f'Entry with id {id} can\'t be upvoted' }, status.HTTP_400_BAD_REQUEST
-            
+
     # using PATCH method to downvote entry
     elif request.method == 'PATCH':
-        down_vote_entry = queries.down_vote_entry(id=id)
+        #down_vote_entry = queries.down_vote_entry(id=id)
+        down_vote_entry = downvoteEntry(r_server,id)
         if down_vote_entry:
              return { 'message': f'Entry with id {id} has been downvoted' }, status.HTTP_200_OK
         else:
@@ -77,6 +98,26 @@ def score_list():
         return list(entries_by_list)
     else:
        return { 'message': 'Posts could not be retrieved' }, status.HTTP_400_BAD_REQUEST
+
+# Upvote an entry
+def upvoteEntry(redisCL, id):
+    hash_check = redisCL.hexists(id,"upvotes")
+    if hash_check:
+        redisCL.hincrby(id,"upvotes",1)
+        return hash_check
+    else:
+        raise exceptions.NotFound()
+
+
+# Downvote an entry:
+def downvoteEntry(redisCL, id):
+    hash_check = redisCL.hexists(id,"downvotes")
+    if hash_check:
+        redisCL.hincrby(id,"downvotes",1)
+        return hash_check
+    else:
+        raise exceptions.NotFound()
+
 
 # Return entry given an id
 def get_entry_with_id(id):
