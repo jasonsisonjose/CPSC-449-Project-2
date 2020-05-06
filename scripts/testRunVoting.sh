ecea82d57cdad6d77b57f53a443603bf796ca805
Jason Jose <jasonsisonjose@csu.fullerton.edu>
2020-05-05 17:47:57 -0700

    fixed decimal error, after a million years

---
+++ scripts/testRunVoting.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+curl --verbose \
+     --request POST \
+     --header 'Content-Type: application/json' \
+     --data @identifierList.json \
+     http://localhost:5100/api/v1/votes/scorelist
+
+curl --verbose \
+     --request GET \
+     http://localhost:5100/api/v1/votes/1
+
+curl --verbose \
+     --request PUT \
+     http://localhost:5100/api/v1/votes/1
+
+curl --verbose \
+     --request PATCH \
+     http://localhost:5100/api/v1/votes/1
